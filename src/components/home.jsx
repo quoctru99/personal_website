f8ab4e3d0e31608f9a27a20e2a0b2eff67db7300
quoctru99 <nguyenquoctru99@gmail.com>
2019-05-09 17:14:21 -0500

    email changes

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -5,10 +5,6 @@ import '../styles/home.sass'
 
 export default class Home extends Component {
 
-    constructor (props) {
-        super(props); 
-    }
-
     render () {
         return (
             <div className="mainground">
